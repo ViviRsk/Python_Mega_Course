9c06fb400d2049d78ee9513bceb2e97eb6e4566a
Vivi Rsk <viva.ruska@gmail.com>
2016-08-10 00:28:07 +0200

    Update with requirements of Coding_Exercise_3
    In exercise 1 you created a function that converted Celsius degrees to Fahrenheit. 
    
    The lowest possible temperature that physical matter can reach is -273.15 °C. With that in mind, please improve the function by making it print out a message in case a number lower than -273.15 is passed as input when calling the function.

--- Data_Types/Custom_Functions.py
+++ Data_Types/Custom_Functions.py
@@ -1,7 +1,12 @@
 def celsius_converter(c):
-  rate = 1.8
-  suf = 32
-  f = c*rate+suf
-  return f
-  
-print(celsius_converter(25))
+
+    rate = 1.8
+    suf = 32
+    f = c*rate+suf
+
+    if c < -273.15:
+        return str(f)+": "+"The lowest possible temperature that physical matter can reach is -273.15 C. Keep that in mind."
+    return f
+
+
+print(celsius_converter(-375))
