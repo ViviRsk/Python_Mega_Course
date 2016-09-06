7971e16181095d6deaa93b7d2f7e9c17a4b7018a
Vivi Rsk <viva.ruska@gmail.com>
2016-09-06 15:51:51 +0200

    Adding Markers to the Map from CSV data

--- App2_Leaflet_Webmaps/map.py
+++ App2_Leaflet_Webmaps/map.py
@@ -1,2 +1,15 @@
-map = folium.Map(location=[45.372,-121.697],zoom_start=12)
+import folium
+import pandas
+
+# store the data in a variable
+df = pandas.read_csv("Volcanoes-USA.txt")
+
+# we create an object
+map = folium.Map(location=[45.372,-121.697],zoom_start=4,tiles='Stamen Terrain')
+
+for lat,lon,name in zip(df['LAT'],df['LON'],df['NAME']):
+    # the section where we add markers
+    map.simple_marker(location=[lat,lon],popup=name,marker_color='red')
+
+# where we create the HTML map
 map.create_map(path='test.html')
