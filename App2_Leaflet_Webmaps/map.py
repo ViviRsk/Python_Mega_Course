7efe7e2e21162cf71a2914ab06878a444b0b04e8
Vivi Rsk <viva.ruska@gmail.com>
2016-09-08 11:53:35 +0200

    Rule-based Coloring of Markers

--- App2_Leaflet_Webmaps/map.py
+++ App2_Leaflet_Webmaps/map.py
@@ -7,9 +7,9 @@ df = pandas.read_csv("Volcanoes-USA.txt")
 # we create an object
 map = folium.Map(location=[45.372,-121.697],zoom_start=4,tiles='Stamen Terrain')
 
-for lat,lon,name in zip(df['LAT'],df['LON'],df['NAME']):
+for lat,lon,name,elev in zip(df['LAT'],df['LON'],df['NAME'],df['ELEV']):
     # the section where we add markers
-    map.simple_marker(location=[lat,lon],popup=name,marker_color='red')
+    map.simple_marker(location=[lat,lon],popup=name,marker_color='green' if elev in range(0,1000) else 'orange' if elev in range(1000,3000) else 'red')
 
 # where we create the HTML map
 map.create_map(path='test.html')
