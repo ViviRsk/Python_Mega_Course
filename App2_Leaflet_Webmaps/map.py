e31dc50e95bbdeeb0f330ef9ebadfebbc6e066d0
Vivi Rsk <viva.ruska@gmail.com>
2016-09-21 23:35:00 +0200

    Adding a Coropleth Map from GeoJson

--- App2_Leaflet_Webmaps/map.py
+++ App2_Leaflet_Webmaps/map.py
@@ -5,7 +5,7 @@ import pandas
 df = pandas.read_csv("Volcanoes-USA.txt")
 
 # we create an object
-map = folium.Map(location=[df['LAT'].mean(),df['LON'].mean()],zoom_start=6,tiles='Stamen Terrain')
+map = folium.Map(location=[df['LAT'].mean(),df['LON'].mean()],zoom_start=6,tiles='Mapbox bright')
 
 
 def color(elev):
@@ -22,6 +22,11 @@ def color(elev):
 for lat,lon,name,elev in zip(df['LAT'],df['LON'],df['NAME'],df['ELEV']):
     # the section where we add markers
     map.add_child(folium.Marker(location=[lat,lon],popup=name,icon=folium.Icon(color=color(elev),icon_color='green')))
+
+map.add_child(folium.GeoJson(data=open('World_population.geojson'),
+                             name='World Population',
+                             style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005']<=10000000 else 'orange' if 10000000<x['properties']['POP2005']<20000000 else 'red'}))
+
 
 # where we create the HTML map
 map.save(outfile='test.html')
