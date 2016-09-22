788f77db809ef4a24100886e6defbb5fef49e2ff
Vivi Rsk <viva.ruska@gmail.com>
2016-09-22 21:43:04 +0200

    Adding a Layer Control Panel
    - adding layer control panel
    - instead of adding the markers to the map, add them to the fg variable

--- App2_Leaflet_Webmaps/map.py
+++ App2_Leaflet_Webmaps/map.py
@@ -19,14 +19,19 @@ def color(elev):
         col = 'red'
     return col
 
+fg = folium.FeatureGroup(name="Volcano Locations")
+
 for lat,lon,name,elev in zip(df['LAT'],df['LON'],df['NAME'],df['ELEV']):
     # the section where we add markers
-    map.add_child(folium.Marker(location=[lat,lon],popup=name,icon=folium.Icon(color=color(elev),icon_color='green')))
+    fg.add_child(folium.Marker(location=[lat,lon],popup=name,icon=folium.Icon(color=color(elev),icon_color='green')))
+
+map.add_child(fg)
 
 map.add_child(folium.GeoJson(data=open('World_population.geojson'),
                              name='World Population',
                              style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005']<=10000000 else 'orange' if 10000000<x['properties']['POP2005']<20000000 else 'red'}))
 
+map.add_child(folium.LayerControl())
 
 # where we create the HTML map
 map.save(outfile='test.html')
