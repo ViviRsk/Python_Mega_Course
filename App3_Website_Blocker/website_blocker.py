004a32e412da40100c8fa90bca026b197d1c4907
Vivi Rsk <viva.ruska@gmail.com>
2016-10-13 02:45:56 +0200

    Update and rename website_blocker,py to website_blocker.py

--- App3_Website_Blocker/website_blocker.py
+++ App3_Website_Blocker/website_blocker.py
@@ -7,7 +7,7 @@ redirect = "127.0.0.1"
 website_list = ["www.facebook.com","facebook.com","mail.google.com/mail/u/0/#inbox","www.mail.google.com/mail/u/0/#inbox"]
 
 while True:
-    if dt(dt.now().year,dt.now().month,dt.now().day,2) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,17):
+    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
         print("Working hours...")
         with open(hosts_path,'r+') as file:
             content = file.read()
