47d26dc2832602babc722c87d2efc7d058d3416b
Vivi Rsk <viva.ruska@gmail.com>
2016-10-19 00:00:13 +0200

    Returning HTML Templates
    How to generate HTML pages using your Python we app - for further info see
    - home.html
    - about.html

--- App4_Website_with_Flask/flask_website.py
+++ App4_Website_with_Flask/flask_website.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Homepage here!"
+    return render_template("home.html")
 
 
 @app.route('/about/')
 def about():
-    return "About content goes here!"
+    return render_template("about.html")
 
 
 if __name__=="__main__":
